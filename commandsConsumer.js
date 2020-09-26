3b64ada3b885f6eff9e7776435fca1cd7b5959a0
Lawrence Torres <torres.lawrence.986@gmail.com>
2020-09-26 11:56:23 +0400

    fixes

--- commandsConsumer.js
+++ commandsConsumer.js
@@ -120,31 +120,13 @@ const execCmd = async (device_id, command_param) => {
   return await sendCommand();
 };
 
-const commandsQ = "commands";
-let channel = [];
-
-const createConn = async () => {
-  try {
-    const conn = await amqp.connect("amqp://ekar:11223344@localhost");
-    const channel = await conn.createChannel();
-
-    return { channel: channel };
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const createChannel = async () => {
-  let ch = await createConn();
-  channel.push(ch.channel);
-};
-
-createChannel();
-
+const q = "commands";
 const consumeMsg = async () => {
   try {
-    const result = await channel[0].assertQueue(commandsQ);
-    channel[0].consume(commandsQ, async (msg) => {
+    const connection = await amqp.connect("amqp://ekar:11223344@localhost");
+    const channel = await connection.createChannel();
+    const result = await channel.assertQueue(q);
+    channel.consume(q, async (msg) => {
       if (msg !== null) {
         let parsedMessage = JSON.parse(msg.content.toString());
         let imei_no = parsedMessage.device_id;
@@ -164,9 +146,7 @@ const consumeMsg = async () => {
           channel.ack(msg);
           console.log("Message Acknowledge!");
         } else {
-          console.log(
-            "Oops! Something went wrong! Please contact Support for details"
-          );
+          console.log("Taihen desu!");
         }
       }
     });
